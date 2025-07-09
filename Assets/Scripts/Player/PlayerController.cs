38a893ba790fea38cb20f1034d63b81e793d1db8
duongba <duongnguyenba385@gmail.com>
2025-07-09 22:25:09 +0700

    PlayerSword
    -Sword
    -SlashAnim

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    private Vector2 movement;
+    private Vector2 lastMoveDir;
+
+    public bool FacingLeft { get { return facingLeft; } set { facingLeft = value; } }
+    private bool facingLeft = false;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        HandleInput();
+        UpdateFacingDirection();
+        UpdateAnimator();
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+    private void UpdateFacingDirection()
+    {
+        if (movement.x < 0)
+            facingLeft = true;
+        else if (movement.x > 0)
+            facingLeft = false;
+    }
+    private void HandleInput()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement != Vector2.zero)
+            lastMoveDir = movement;
+    }
+    private void UpdateAnimator()
+    {
+        animator.SetFloat("moveX", movement.x);
+        animator.SetFloat("moveY", movement.y);
+        animator.SetFloat("lastMoveX", lastMoveDir.x);
+        animator.SetFloat("lastMoveY", lastMoveDir.y);
+        animator.SetBool("isMoving", movement != Vector2.zero);
+    }
+}
--- Assets/Scripts/Weapon/Sword.cs
+++ Assets/Scripts/Weapon/Sword.cs
@@ -1,16 +1,68 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject slashAnimPrefab;
+    [SerializeField] private Transform slashAnimSpawnPoint;
 
-    // Update is called once per frame
+    private GameObject slashAnim;
+    private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
+
+    void Awake()
+    {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
+        myAnimator = GetComponent<Animator>();
+    }
     void Update()
     {
-        
+        MouseFollowWithOffset();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+    private void Attack()
+    {
+        myAnimator.SetTrigger("Attack");
+
+        slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity);
+        slashAnim.transform.parent = this.transform.parent;
+
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPos = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        SpriteRenderer sr = slashAnim.GetComponent<SpriteRenderer>();
+        if (sr != null)
+        {
+            sr.flipX = (mousePos.x < playerScreenPos.x);
+        }
+    }
+    public void SwingUpFlipAnimEvent()
+    {
+        if (slashAnim == null) return;
+        slashAnim.transform.rotation = Quaternion.Euler(-180, 0, 0);
+    }
+    public void SwingDownFlipAnimEvent()
+    {
+        if (slashAnim == null) return;
+        slashAnim.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, 0);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
