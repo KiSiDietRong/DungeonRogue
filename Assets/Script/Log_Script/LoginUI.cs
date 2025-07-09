f7830377b393cc7e4125987cc906f7d98d9cb0fe
KiSiDietRong <hnguyendac942@gmail.com>
2025-07-09 10:59:13 +0700

    Register/Login
    - Add register, Login and Setting UI Button
    - Code register, Login using Json to saved

---
+++ Assets/Script/Log_Script/LoginUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginUI : MonoBehaviour
+{
+    public InputField usernameInput;
+    public InputField passwordInput;
+    public Text messageText;
+    public UserManager userManager;
+
+    public void OnRegisterClick()
+    {
+        string username = usernameInput.text;
+        string password = passwordInput.text;
+
+        string result = userManager.Register(username, password);
+        messageText.text = result;
+    }
+
+    public void OnLoginClick()
+    {
+        string username = usernameInput.text;
+        string password = passwordInput.text;
+
+        string result = userManager.Login(username, password);
+        messageText.text = result;
+
+        if (result == "Login Success!")
+        {
+            // Chuyển cảnh hoặc vào game
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
---
+++ Assets/Script/Log_Script/UserManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class UserManager : MonoBehaviour
+{
+    private string savePath;
+    private Dictionary<string, string> userDatabase = new Dictionary<string, string>();
+
+    private void Awake()
+    {
+        savePath = Application.persistentDataPath + "/users.json";
+        LoadUsers();
+    }
+
+    // Hàm đăng ký
+    public string Register(string username, string password)
+    {
+        if (username.Length > 12)
+            return "Username can long than 12 sentences.";
+
+        if (userDatabase.ContainsKey(username))
+            return "Account already exist.";
+
+        userDatabase.Add(username, password);
+        SaveUsers();
+        return "Register Success!";
+    }
+
+    // Hàm đăng nhập
+    public string Login(string username, string password)
+    {
+        if (userDatabase.ContainsKey(username))
+        {
+            if (userDatabase[username] == password)
+                return "Login Success!";
+            else
+                return "Wrong password.";
+        }
+        return "Invalid account.";
+    }
+
+    private void SaveUsers()
+    {
+        List<UserData> userList = new List<UserData>();
+        foreach (var pair in userDatabase)
+        {
+            userList.Add(new UserData { username = pair.Key, password = pair.Value });
+        }
+        string json = JsonUtility.ToJson(new UserListWrapper { users = userList }, true);
+        Debug.Log("Save JSON: " + json);
+        File.WriteAllText(savePath, json);
+    }
+
+    private void LoadUsers()
+    {
+        if (!File.Exists(savePath)) return;
+
+        string json = File.ReadAllText(savePath);
+        UserListWrapper wrapper = JsonUtility.FromJson<UserListWrapper>(json);
+        userDatabase.Clear();
+
+        if (wrapper != null && wrapper.users != null)
+        {
+            foreach (var user in wrapper.users)
+            {
+                userDatabase[user.username] = user.password;
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class UserListWrapper
+{
+    public List<UserData> users = new List<UserData>();
+}
