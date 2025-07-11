4685466452eb33f67a8fb9d4d130c7d08f5afa7c
duongba <duongnguyenba385@gmail.com>
2025-07-11 22:45:46 +0700

    TestFlip
    -TestFlip(Staff,Bow,Sword)
    -ScriptableObject

--- Assets/Scripts/Weapon/Bow.cs
+++ Assets/Scripts/Weapon/Bow.cs
@@ -4,6 +4,22 @@ public class Bow : MonoBehaviour, IWeapon
 {
     public void Attack()
     {
-        Debug.Log("Bow Attack");
+        Debug.Log("Staff Attack");
+    }
+
+    private void Update()
+    {
+        MouseFollowWithOffset();
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3 dir = worldMousePos - transform.position;
+        dir.z = 0f;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
--- Assets/Scripts/Weapon/Staff.cs
+++ Assets/Scripts/Weapon/Staff.cs
@@ -6,4 +6,20 @@ public class Staff : MonoBehaviour, IWeapon
     {
         Debug.Log("Staff Attack");
     }
+
+    private void Update()
+    {
+        MouseFollowWithOffset();
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3 dir = worldMousePos - transform.position;
+        dir.z = 0f;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
 }
