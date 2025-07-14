44430d3b7bb7e3e99a3b68172f7d7c0a359d1c32
duongba <duongnguyenba385@gmail.com>
2025-07-14 23:15:36 +0700

    UIHealthPlayer
    -FixSlider
    -PlayerHealthUIChanger

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,13 +1,20 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [Header("Config")]
     [SerializeField] private float knockbackThrustAmount = 10f;
     [SerializeField] private float damageRecoveryTime = 1f;
 
+    [Header("UI References")]
+    [SerializeField] private Slider hpSlider;
+    [SerializeField] private TextMeshProUGUI hpText;
+
     private int currentHealth;
     private int maxHealth;
-
     private bool canTakeDamage = true;
 
     private Knockback knockback;
@@ -26,6 +33,15 @@ public class PlayerHealth : MonoBehaviour
         characterStat = stats;
         maxHealth = Mathf.RoundToInt(characterStat.maxHealth);
         currentHealth = maxHealth;
+        InitUI();
+    }
+
+    private void InitUI()
+    {
+        if (hpSlider != null)
+            hpSlider.maxValue = maxHealth;
+
+        UpdateUI();
     }
 
     public void TakeDamage(int damageAmount, Transform hitTransform)
@@ -35,9 +51,35 @@ public class PlayerHealth : MonoBehaviour
         canTakeDamage = false;
         currentHealth -= damageAmount;
 
+        knockback?.GetKnockedBack(hitTransform, knockbackThrustAmount);
+
+        UpdateUI();
+
         if (currentHealth <= 0)
         {
+            Die();
         }
 
+        StartCoroutine(ResetDamageRecovery());
+    }
+
+    private IEnumerator ResetDamageRecovery()
+    {
+        yield return new WaitForSeconds(damageRecoveryTime);
+        canTakeDamage = true;
+    }
+
+    private void UpdateUI()
+    {
+        if (hpSlider != null)
+            hpSlider.value = currentHealth;
+
+        if (hpText != null)
+            hpText.text = $"{currentHealth} / {maxHealth}";
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player died");
     }
 }
