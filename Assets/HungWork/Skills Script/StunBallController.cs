1a1b75d814cd3d295ccdc7b4e4cb82759d28e8e6
hungdtvcl <letuanhung05@gmail.com>
2025-08-03 00:31:06 +0700

    skill buff atk + stun

---
+++ Assets/HungWork/Skills Script/StunBallController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class StunBallController : MonoBehaviour
+{
+    private Vector2 direction;
+    private float speed;
+    private float stunDuration;
+    private int damage;
+
+    public void Initialize(Vector2 dir, float spd, float stunTime, int dmg)
+    {
+        direction = dir;
+        speed = spd;
+        stunDuration = stunTime;
+        damage = dmg;
+
+        Destroy(gameObject, 5f); // tự huỷ nếu không trúng gì
+    }
+
+    void Update()
+    {
+        transform.position += (Vector3)(direction * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+                enemy.Stun(stunDuration);
+            }
+
+            Destroy(gameObject); // huỷ khi trúng kẻ địch
+        }
+    }
+}
--- Assets/Scripts/Enemy/EnemyBlackHolePull.cs
+++ Assets/Scripts/Enemy/EnemyBlackHolePull.cs
@@ -1,30 +1,21 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class EnemyBlackHolePull : MonoBehaviour
 {
     private bool beingPulled = false;
     private Vector2 pullTarget;
-    private float pullSpeed = 5f;
+    private float pullSpeed;
 
-    private Rigidbody2D rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    void LateUpdate()
+    void Update()
     {
         if (beingPulled)
         {
-            Vector2 direction = (pullTarget - rb.position).normalized;
-            Vector2 newPosition = rb.position + direction * pullSpeed * Time.deltaTime;
-            rb.MovePosition(newPosition);
+            Vector2 dir = (pullTarget - (Vector2)transform.position).normalized;
+            transform.position += (Vector3)(dir * pullSpeed * Time.deltaTime);
         }
     }
 
-    public void PullTowards(Vector2 center, float speed = 5f)
+    public void PullTowards(Vector2 center, float speed)
     {
         pullTarget = center;
         pullSpeed = speed;
