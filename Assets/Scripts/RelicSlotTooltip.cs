08966c6a710f3461c21d5e5a57c0fcc81e38bc8f
Duck0501 <vanduc050105@gmail.com>
2025-08-05 12:58:46 +0700

    update Inventory

---
+++ Assets/Scripts/RelicSlotTooltip.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RelicSlotTooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public int slotIndex;
+    private InventoryManager inventoryManager;
+
+    void Start()
+    {
+        inventoryManager = InventoryManager.Instance;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        inventoryManager.ShowRelicTooltip(slotIndex, transform.position);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        inventoryManager.HideRelicTooltip();
+    }
+}
