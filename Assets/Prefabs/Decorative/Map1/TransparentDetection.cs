b9c9016b2207293d5ed31bbd3d8e5d4e840b65f4
duongba <duongnguyenba385@gmail.com>
2025-08-12 20:59:55 +0700

    Map,Tree
    -TransparentDetection
    -Fix lỗi layer

---
+++ Assets/Prefabs/Decorative/Map1/TransparentDetection.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TransparentDetection : MonoBehaviour
+{
+    [Range(0, 1)]
+    [SerializeField] private float transparencyAmount = 0.5f; 
+    [SerializeField] private float fadeTime = 0.4f;           
+
+    private SpriteRenderer spriteRenderer;
+    private Coroutine fadeCoroutine;
+    private int originalSortingOrder; 
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalSortingOrder = spriteRenderer.sortingOrder; 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerController>() || other.GetComponent<Enemy>())
+        {
+            StartFade(transparencyAmount);
+            spriteRenderer.sortingOrder = originalSortingOrder + 1; 
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerController>() || other.GetComponent<Enemy>())
+        {
+            StartFade(1f);
+            spriteRenderer.sortingOrder = originalSortingOrder; 
+        }
+    }
+
+    private void StartFade(float targetAlpha)
+    {
+        if (fadeCoroutine != null)
+            StopCoroutine(fadeCoroutine);
+
+        fadeCoroutine = StartCoroutine(FadeRoutine(targetAlpha));
+    }
+
+    private IEnumerator FadeRoutine(float targetAlpha)
+    {
+        float startAlpha = spriteRenderer.color.a;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / fadeTime);
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, newAlpha);
+            yield return null;
+        }
+
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, targetAlpha);
+    }
+}
