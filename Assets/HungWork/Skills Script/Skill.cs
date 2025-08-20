ed399bf6d5d06d708d5c994ded1423344ec1a680
hungdtvcl <letuanhung05@gmail.com>
2025-08-20 15:48:56 +0700

    info

--- Assets/HungWork/Skills Script/Skill.cs
+++ Assets/HungWork/Skills Script/Skill.cs
@@ -4,6 +4,7 @@ public abstract class Skill : ScriptableObject
 {
     [Header("Basic Info")]
     public string skillName = "New Skill";
+    public string skillInfo = "Skill Info";
     public Sprite icon;
 
     [Header("Cooldown")]
